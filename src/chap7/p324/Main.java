4e1292811177617219f1c755e91161bde8188fe6
SOLENAS-NOTE_03\yoshi <yoshikai.tsuyoshi@gmail.com>
2023-05-21 18:43:52 +0900

    chapter7の練習問題を追加

--- src/chap7/p324/Main.java
+++ src/chap7/p324/Main.java
@@ -3,7 +3,7 @@ package chap7.p324;
 public class Main {
     public static void main(String[] args) {
         // 例外処理は任意
-        int i = Integer.parseInt("10");
+//        int i = Integer.parseInt("10");
 
         // 例外処理は必須
         //new FileReader("Test.txt");
